31378cc854fdfcb52e20866a612873d409732c2c
Admin <drashtipatel7080@gmail.com>
2023-08-31 16:57:10 +0530

    31/8/2023

---
+++ src/Second.java
@@ -0,0 +1,13 @@
+public class Second
+{
+    public static void main(String[] args)
+    {
+        System.out.println("  ------");
+        System.out.println("|\t \t |");
+        System.out.println("|\t \t |");
+        System.out.println("  Drashti");
+        System.out.println("|\t \t |");
+        System.out.println("|\t \t |");
+        System.out.println("  -------");
+    }
+}
---
+++ src/Third.java
@@ -0,0 +1,17 @@
+public class Third
+{
+    public static void main(String[] args)
+    {
+        System.out.println("  - - -");
+        System.out.println("|\t\t|");
+        System.out.println("D\t\t|");
+        System.out.println("r\t\t|");
+        System.out.println("a\t\t|");
+        System.out.println("s\t\t|");
+        System.out.println("h\t\t|");
+        System.out.println("t\t\t|");
+        System.out.println("i\t\t|");
+        System.out.println("|\t\t|");
+        System.out.println("  - - -");
+    }
+}
