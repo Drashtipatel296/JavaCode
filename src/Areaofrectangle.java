50bbf6c80ae1b8fb0bd0ea412ed2e1fbd56777a9
Admin <drashtipatel7080@gmail.com>
2023-09-11 22:53:55 +0530

    11/9/2023

---
+++ src/Areaofrectangle.java
@@ -0,0 +1,13 @@
+public class Areaofrectangle
+{
+    public static void main(String[] args)
+    {
+        byte height = 10;
+        byte width = 20;
+        int area;
+
+        area = width*height;
+
+        System.out.println("Area of rectangle is = "+area);
+    }
+}
