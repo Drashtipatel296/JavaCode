b5f32d10b70c0e0893136f364cf34cda3b9cc3b0
Admin <drashtipatel7080@gmail.com>
2023-09-12 09:22:00 +0530

    12/9/2023

---
+++ src/Perimeter.java
@@ -0,0 +1,12 @@
+public class Perimeter
+{
+    public static void main(String[] args)
+    {
+        byte radius = 10;
+        double PI = 3.14;
+
+        double area = 2 * 3.14 * radius;
+
+        System.out.println("Perimeter of circle ="+area);
+    }
+}
---
+++ src/Simpleinterest.java
@@ -0,0 +1,13 @@
+public class Simpleinterest
+{
+    public static void main(String[] args)
+    {
+        int p = 2000;
+        int r = 5;
+        int t = 2;
+
+        int si = (int) (p * r * t)/100;
+
+        System.out.println("Simple interest is = "+si);
+    }
+}
