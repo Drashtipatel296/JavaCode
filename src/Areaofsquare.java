2a03cc0606c5b7a61429a2b8c0e12c6f6a4debed
Admin <drashtipatel7080@gmail.com>
2023-09-12 09:07:15 +0530

    12/9/2023

---
+++ src/Areaofsquare.java
@@ -0,0 +1,11 @@
+public class Areaofsquare
+{
+    public static void main(String[] args)
+    {
+        byte s = 20;
+
+        int area = (int) s * s;
+
+        System.out.println("Area of square ="+area);
+    }
+}
---
+++ src/Areaoftriangle.java
@@ -0,0 +1,11 @@
+public class Areaoftriangle {
+    public static void main(String[] args)
+    {
+        byte b = 12;
+        byte h = 6;
+
+        float area = (float)(b*h)/2;
+
+        System.out.println("Area of triangle ="+area);
+    }
+}
