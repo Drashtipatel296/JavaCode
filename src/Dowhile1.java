3615ad8e428086948cd9885fea0cae65224ff5fd
Admin <drashtipatel7080@gmail.com>
2023-09-20 20:11:05 +0530

    20/9/2023

---
+++ src/Dowhile1.java
@@ -0,0 +1,14 @@
+public class Dowhile1
+{
+    public static void main(String[] args)
+    {
+        byte x = 1;
+
+        do
+        {
+            System.out.println(x);
+            x++;
+        }
+        while(x<=10);
+    }
+}
---
+++ src/Dowhile2.java
@@ -0,0 +1,14 @@
+public class Dowhile2
+{
+    public static void main(String[] args)
+    {
+        byte x = 10;
+
+        do
+        {
+            System.out.println(x);
+            x--;
+        }
+        while(x>=0);
+    }
+}
